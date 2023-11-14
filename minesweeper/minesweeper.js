25215af3991f22399b8194b351a57e94168a6570
hectorhmsp <hectorhmsp0310@gmail.com>
2023-11-13 21:31:31 -0300

    minesweeper project

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -1,47 +1,63 @@
 divBoard = document.getElementById('board');
 
-let rows = 3;
-let cols = 3;
+let rows = 5;
+let cols = 5;
 
 let minesweeperBoard = [
-    [0, 0, 0],
-    [0, 1, 0],
-    [0, 0, 0]
+  [1, 0, 0, 1],
+  [0, 0, 0, 0],
+  [0, 0, 0, 1],
+	[0, 0, 0, 0],
+	[0, 0, 1, 0]
 ];
 
 for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-        let tile = document.createElement('div');
-        tile.classList.add('tile');
+  for (let j = 0; j < cols; j++) {
+    let tile = document.createElement('div');
+    tile.classList.add('tile');
 
-        if (minesweeperBoard[i][j] === 1) {
-            tile.classList.add('mine');
-            tile.innerText = " * "
-        }
+    let bomb = 0;
 
-        checkForMines(i, j, minesweeperBoard, tile);
+    if (minesweeperBoard[i][j] === 1) {
+      tile.classList.add('mine');
+      tile.innerText = " * ";
+    } else {
+      if (j > 0 && minesweeperBoard[i][j - 1] === 1) {
+        bomb += 1;
+      }
+      if (j < minesweeperBoard[i].length - 1 && minesweeperBoard[i][j + 1] === 1) {
+        bomb += 1;
+      }
+      if (i > 0 && minesweeperBoard[i - 1][j] === 1) {
+        bomb += 1;
+      }
+      if (i < minesweeperBoard.length - 1 && minesweeperBoard[i + 1][j] === 1) {
+        bomb += 1;
+      }
+      if (i > 0 && j > 0 && minesweeperBoard[i - 1][j - 1] === 1) {
+        bomb += 1;
+      }
+      if (i < minesweeperBoard.length - 1 && j < minesweeperBoard[i].length - 1 && minesweeperBoard[i + 1][j + 1] === 1) {
+        bomb += 1;
+      }
+      if (i < minesweeperBoard.length - 1 && j > 0 && minesweeperBoard[i + 1][j - 1] === 1) {
+        bomb += 1;
+      }
+      if (i > 0 && j < minesweeperBoard[i].length - 1 && minesweeperBoard[i - 1][j + 1] === 1) {
+        bomb += 1;
+      }
 
-        divBoard.appendChild(tile);
+      // Display bomb count
+      if (bomb > 0) {
+        tile.innerText = `${bomb}`;
+      } else {
+        tile.innerText = "  ";
+      }
     }
+
+    divBoard.appendChild(tile);
+  }
 }
 
-divBoard.style.gridTemplateRows = 'repeat(' + rows + ', 30px)';
-divBoard.style.gridTemplateColumns = 'repeat(' + cols + ', 30px)';
-
-
-function checkForMines(i, j, minesweeperBoard, tile) {
-    const numRows = minesweeperBoard.length;
-    const numCols = minesweeperBoard[0].length;
-    
-    if (minesweeperBoard[i][0] === 0 && minesweeperBoard[i][j] !== 1) {
-        if (
-            (i > 0 && minesweeperBoard[i - 1][0]) ||
-            (i > 0 && minesweeperBoard[i - 1][1]) ||
-            (i > 0 && minesweeperBoard[i][1]) ||
-            (i < numRows - 1 && minesweeperBoard[i + 1][0]) ||
-            (i < numRows - 1 && minesweeperBoard[i + 1][1]) === 1
-        ) {
-            tile.innerText = " 1 ";
-        }
-    }
-}
+divBoard.style.gridTemplateRows = 'repeat(' + rows + ', 80px)';
+divBoard.style.gridTemplateColumns = 'repeat(' + cols + ', 80px)';
