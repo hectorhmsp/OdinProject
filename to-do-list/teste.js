6ca8b532c99b437f40954fbc2fef854698beca1f
hectorhmsp <hectorhmsp0310@gmail.com>
2024-03-24 11:22:51 -0300

    changes

--- to-do-list/teste.js
+++ to-do-list/teste.js
@@ -27,3 +27,33 @@ function addImportantTask (isTaskImportant) {
 
     return importantTask;
 }
+
+function addRemoveTask (task, project, taskDiv, specificTask) {
+    const removeTask = document.createElement('div');
+    removeTask.innerHTML = `<img src="cancel.svg" alt="Ícone SVG" class="cancel-svg" />`;
+    removeTask.classList.add('remove-task');
+    removeTask.addEventListener('click', () => {
+        const taskIndex = project.tasks.indexOf(task);
+
+        if (taskIndex !== -1) { project.tasks.splice(taskIndex, 1) }
+
+        taskDiv.removeChild(specificTask);
+        saveTasksToLocal();
+    });  
+
+    return removeTask;
+}
+
+function specificTaskAppend(specificTask, div, dateInput, removeTask, importantTask) {
+    specificTask.appendChild(div);
+    specificTask.appendChild(dateInput);
+    specificTask.appendChild(removeTask);
+    specificTask.appendChild(importantTask);
+}
+
+function specificTaskRemove(specificTask, newDiv, dateInput, importantTask, removeTask) {
+    specificTask.removeChild(newDiv);
+    specificTask.removeChild(dateInput);
+    specificTask.removeChild(importantTask);
+    specificTask.removeChild(removeTask);
+}
