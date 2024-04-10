1eb63f1d8185fb117a5ac656f1a559a3a3564d14
hectorhmsp <hectorhmsp0310@gmail.com>
2024-04-10 17:50:24 -0300

    .

---
+++ api-tests/async-await-test.js
@@ -0,0 +1,56 @@
+const server = {
+  people: [
+    {
+      name: "Odin",
+      age: 20,
+    },
+    {
+      name: "Thor",
+      age: 35,
+    },
+    {
+      name: "Freyja",
+      age: 29,
+    },
+  ],
+
+  getPeople() {
+    return new Promise((resolve, reject) => {
+      // Simulating a delayed network call to the server
+      setTimeout(() => {
+        resolve(this.people);
+      }, 1000);
+    });
+  },
+};
+
+async function getPersonsInfo(name) {
+  const people = await server.getPeople();
+  const person = people.find(person => { return person.name === name });
+  return person;
+}
+
+async function fetchData(name) {
+  try {
+    const person = await getPersonsInfo(name);
+    console.log("Person found:", person);
+    return person;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+fetchData("Thor");
+
+/* 
+or:
+
+getPersonsInfo("Thor")
+  .then(person => {
+    console.log("Person found:", person);
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+  });
+
+*/
