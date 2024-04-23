daa07a25ae52ca9fb6bf35103a47127d7c3f0a25
hectorhmsp <hectorhmsp0310@gmail.com>
2024-04-23 16:53:59 -0300

    .

--- weather-app/script.js
+++ weather-app/script.js
@@ -18,6 +18,8 @@ const today = document.querySelector('.today');
 const tomorrow = document.querySelector('.tomorrow');
 const overmorrow = document.querySelector('.overmorrow');
 
+
+
 async function getData(inputValue) {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=a8361c2199b2472e859114108241004&q=${inputValue}`, { mode: 'cors' });
     const responseData = await response.json();
@@ -38,6 +40,29 @@ async function getData(inputValue) {
         console.log(forecastData.forecast.forecastday[1]);
         console.log(forecastData.forecast.forecastday[2]);
 
+        function createAndSetElement(tagName, textContent, id) {
+            const element = document.createElement(tagName);
+            if (textContent) {
+                element.innerText = textContent;
+            }
+            if (id) {
+                element.id = id;
+            }
+            return element;
+        }
+        
+        const cityElement = createAndSetElement('p', `${responseData.location.name}`, 'cityElement');
+        const stateElement = createAndSetElement('p', `${responseData.location.region}`, 'stateElement');
+        const lastUpdatedElement = createAndSetElement('p', `Last Updated: ${responseData.current.last_updated}`, 'lastUpdatedElement');
+        const temperatureElement = createAndSetElement('p', `${responseData.current.temp_c} ºC`, 'temperatureElement');
+        const windSpeedElement = createAndSetElement('p', `Wind Speed: ${responseData.current.wind_kph} km/h`, 'windSpeedElement');
+        const windDirectionElement = createAndSetElement('p', `Wind Direction: ${responseData.current.wind_dir}`, 'windDirectionElement');
+        const pressureElement = createAndSetElement('p', `Pressure: ${responseData.current.pressure_mb} mb`, 'pressureElement');
+        const precipitationElement = createAndSetElement('p', `Precipitation: ${responseData.current.precip_mm} mm`, 'precipitationElement');
+        const humidityElement = createAndSetElement('p', `Humidity: ${responseData.current.humidity}%`, 'humidityElement');
+        const cloudCoverElement = createAndSetElement('p', `Cloud cover: ${responseData.current.cloud}%`, 'cloudCoverElement');
+        const feelsLikeElement = createAndSetElement('p', `FeelsLike: ${responseData.current.feelslike_c} ºC`, 'feelsLikeElement');
+        
         hoursDiv.innerHTML = '';
 
         for (let i=0; i<(forecastData.forecast.forecastday[0].hour).length; i++) {
@@ -55,18 +80,39 @@ async function getData(inputValue) {
         overmorrow.innerHTML = `Overmorrow: <br> <br> ${forecastData.forecast.forecastday[2].day.maxtemp_c} /
             ${forecastData.forecast.forecastday[2].day.mintemp_c}`;
 
-        const locationElement = document.createElement('p');
-        locationElement.innerHTML = `City: ${responseData.location.name}<br> 
-            State: ${responseData.location.region};<br>
-            Last Updated: ${responseData.current.last_updated}<br>
-            Temperature: ${responseData.current.temp_c}°C<br>
-            Wind Speed: ${responseData.current.wind_kph} km/h<br>
-            Wind Direction: ${responseData.current.wind_dir}<br>
-            Pressure: ${responseData.current.pressure_mb} mb<br>
-            Precipitation: ${responseData.current.precip_mm} mm<br>
-            Humidity: ${responseData.current.humidity}%<br>
-            Cloud Cover: ${responseData.current.cloud}%<br>
-            Feels Like: ${responseData.current.feelslike_c}°C`;
+        const mainElements = document.createElement('div');
+        const secundaryElements = document.createElement('div');
+
+        locationDiv.appendChild(mainElements);
+        locationDiv.appendChild(secundaryElements);
+
+        
+        mainElements.appendChild(cityElement);
+        mainElements.appendChild(stateElement);
+        mainElements.appendChild(temperatureElement);
+        mainElements.appendChild(lastUpdatedElement);
+        
+        secundaryElements.appendChild(windSpeedElement);
+        secundaryElements.appendChild(windDirectionElement);
+        secundaryElements.appendChild(pressureElement);
+        secundaryElements.appendChild(precipitationElement);
+        secundaryElements.appendChild(humidityElement);
+        secundaryElements.appendChild(cloudCoverElement);
+        secundaryElements.appendChild(feelsLikeElement);
+
+            /*
+            City: ${cityElement.innerText}<br> 
+            State: ${stateElement.innerText};<br>
+            Last Updated: ${lastUpdatedElement.innerText}<br>
+            Temperature: ${temperatureElement.innerText}°C<br>
+            Wind Speed: ${windSpeedElement.innerText} km/h<br>
+            Wind Direction: ${windDirectionElement.innerText}<br>
+            Pressure: ${pressureElement.innerText} mb<br>
+            Precipitation: ${precipitationElement.innerText} mm<br>
+            Humidity: ${humidityElement.innerText}%<br>
+            Cloud Cover: ${cloudCoverElement.innerText}%<br>
+            Feels Like: ${feelsLikeElement.innerText}°C`;
+            */
 
         locationDiv.appendChild(locationElement);
 
